ce9806f4552f4af68b6c6b317d498fd66fd9a6ff
Ben Hailu <b.hailu.pk@gmail.com>
2022-06-06 16:42:16 -0400

    update add function, add subtract function -change display to html

--- js.js
+++ js.js
@@ -1,22 +1,31 @@
 function setUpEvents(){
+    // add
     const sumBtn = document.getElementById("sumEquals");
     const displaySum = document.getElementById("displaySum");
+
+    // subtract
+    const diffBtn = document.getElementById("diffEquals")
+    const displayDiff=document.getElementById("displayDiff");
 
     sumBtn.addEventListener('click', function(){
         add();
     });
+    diffBtn.addEventListener('click', function(){
+        subtract();
+    });
+
     function add() {
         let a = document.getElementById("addendA").value;
         let b = document.getElementById("addendB").value;
         let sum = parseInt(a)+parseInt(b);
-        displaySum.innerHTML+=sum;
-        // let arr = [a,b];
-        // let result = arr.reduce((sum, current) => sum + current, 0);
-        // alert(result);               
+        displaySum.innerHTML="Sum: "+sum;             
     };
 
     function subtract(){
-        
+        let a = document.getElementById("subtractA").value;
+        let b = document.getElementById("subtractB").value;
+        let diff = parseInt(a)-parseInt(b);
+        displayDiff.innerHTML="Difference: "+diff;
     }
 
     function multiply(){
